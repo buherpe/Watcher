b785813eca39b4b40b4dc1547496127f9c5c7bd5
BuHerPeT <buherpet.38@gmail.com>
2019-12-08 20:52:15 +0500

    +icon

--- Watcher/Properties/Resources.Designer.cs
+++ Watcher/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Watcher.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon binoculars {
+            get {
+                object obj = ResourceManager.GetObject("binoculars", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
